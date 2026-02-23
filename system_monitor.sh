c323bf5b9cdd0a288fd3676a4d7b0b8ce17a087a
AlirezaArdani <alireza.ardani.01@gmail.com>
2026-02-23 10:47:20 +0330

    nginx installation implemented. check if not installed then if not install it or if install output the version. then output installation logs and output version. yet to go

--- system_monitor.sh
+++ system_monitor.sh
@@ -28,15 +28,15 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-print_info "${PURPLE}========== CPU Information ===========${NC}\n"
+print_info "${PURPLE}=============== CPU Information ===============${NC}\n"
 lscpu | head -20
 echo "" 
-print_info "${PURPLE}========== Process Status Report ==========${NC}\n"
+print_info "${PURPLE}=============== Process Status Report ===============${NC}\n"
 echo ""
 top -bn1 | grep "Cpu(s)"
 echo ""
 # --- Display CPU Metrics Explanation Table ---
-print_info "${CYAN}=============== CPU Metrics Explanation ==========${NC}\n"
+print_info "${CYAN}=============== CPU Metrics Explanation ===============${NC}\n"
 printf "%-10s %-15s %-40s\n" "Metric" "Full Name" "Description"
 printf "%-10s %-15s %-40s\n" "------" "---------" "-----------"
 printf "%-10s %-15s %-40s\n" "us" "User" "Time spent on user applications"
@@ -48,14 +48,42 @@ printf "%-10s %-15s %-40s\n" "hi" "Hardware IRQ" "Time servicing hardware interr
 printf "%-10s %-15s %-40s\n" "si" "Software IRQ" "Time servicing software interrupts"
 printf "%-10s %-15s %-40s\n" "st" "Steal" "Time stolen by hypervisor (VMs only)"
 echo ""
-print_info "${PURPLE}========== RAM Usage Information ===========${NC}\n"
+print_info "${PURPLE}===============RAM Usage Information ===============${NC}\n"
 free -h
 echo ""
-print_info "${PURPLE}========== DISK Space Information ===========${NC}\n"
+print_info "${PURPLE}=============== DISK Space Information ===============${NC}\n"
 lsblk 
 echo "------------------------------------------------------------------"
 df -h
 echo ""
-print_success "${GREEN}========== System Resources Checked ===========${NC}\n"
+print_success "${GREEN}=============== System Resources Checked ===============${NC}\n"
 
 echo -e "Starting NginX Service...............\n" 
+print_info "${PURPLE}=============== Nginx Installation Check ===============${NC}"
+
+# check id nginx installed or not
+if command -v nginx $> /dev/null; then
+    print_success "NginX is already installed!"
+    NGINX_VERSION=$(nginx -v 2>&1)
+    print_info "${CYAN}Version: $NGINX_VERSION${NC}"
+else
+    print_info "NginX is not installed. Starting installation..."
+    apt-get update > /dev/null 
+    
+    if apt-get install -y nginx; then
+    	nginx -v
+    	print_success "${GREEN}NginX installed successfully.${NC}" 	
+    else
+    	print_error "${RED} Failed to install NginX${NC}"
+    	exit 1
+    fi
+fi
+# --- Create Simple HTML Page ---
+print_info "${PURPLE}=============== Deploying HTML Page ===============${NC}"
+
+
+
+
+
+
+
