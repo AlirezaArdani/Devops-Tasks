52e61c33a5faba9c3a8a0756fd9e0f68f7e5d211
AlirezaArdani <alireza.ardani.01@gmail.com>
2026-02-23 10:09:09 +0330

    resource commands implemented completely. disk information checked including block devices and disk space.

--- system_monitor.sh
+++ system_monitor.sh
@@ -28,15 +28,15 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-print_info "${PURPLE}========== CPU Information ===========${NC}"
+print_info "${PURPLE}========== CPU Information ===========${NC}\n"
 lscpu | head -20
 echo "" 
-print_info "${PURPLE}========== Process Status Report ==========${NC}"
+print_info "${PURPLE}========== Process Status Report ==========${NC}\n"
 echo ""
 top -bn1 | grep "Cpu(s)"
 echo ""
 # --- Display CPU Metrics Explanation Table ---
-print_info "${CYAN}=============== CPU Metrics Explanation ==========${NC}"
+print_info "${CYAN}=============== CPU Metrics Explanation ==========${NC}\n"
 printf "%-10s %-15s %-40s\n" "Metric" "Full Name" "Description"
 printf "%-10s %-15s %-40s\n" "------" "---------" "-----------"
 printf "%-10s %-15s %-40s\n" "us" "User" "Time spent on user applications"
@@ -48,8 +48,14 @@ printf "%-10s %-15s %-40s\n" "hi" "Hardware IRQ" "Time servicing hardware interr
 printf "%-10s %-15s %-40s\n" "si" "Software IRQ" "Time servicing software interrupts"
 printf "%-10s %-15s %-40s\n" "st" "Steal" "Time stolen by hypervisor (VMs only)"
 echo ""
-print_info "${PURPLE}========== RAM Usage Information ===========${NC}"
+print_info "${PURPLE}========== RAM Usage Information ===========${NC}\n"
 free -h
 echo ""
-print_info "${PURPLE}========== DISK Space Information ===========${NC}"
+print_info "${PURPLE}========== DISK Space Information ===========${NC}\n"
+lsblk 
+echo "------------------------------------------------------------------"
+df -h
+echo ""
+print_success "${GREEN}========== System Resources Checked ===========${NC}\n"
 
+echo -e "Starting NginX Service...............\n" 
