7199990dfbd672c464727dc6215bc320165c3e9e
AlirezaArdani <alireza.ardani.01@gmail.com>
2026-02-23 09:49:44 +0330

    implement cpu and process overall status in bash finished. 3 function implemented to show success - error - info text.

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+echo -e "Fetching System Information...............\n" 
 
-# color codes 
+
+
+# --- Color Codes for Output ---
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m'
+NC='\033[0m' # No Color
 
-echo -e "$ CPU Information"
-lscpu
+
+# Function to print status messages
+print_info() {
+    echo -e "${YELLOW}[INFO]${NC} $1"
+}
+print_success() {
+    echo -e "${GREEN}[SUCCESS]${NC} $1"
+}
+print_error() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
+# Check if running as root
+if [ "$EUID" -ne 0 ]; then 
+    print_error "Please run as root (use sudo)"
+    exit 1
+fi
+
+print_info "=============== CPU and RAM Information ==========="
+lscpu | head -20
 echo "" 
-echo "===================="
-echo -e  "${GREEN} System Status Report " 
-echo "===================="
-echo 
-top -n 10
+print_info "=============== Process Status Report =========="
 echo ""
-
-
+top -bn1 | grep "Cpu(s)"
+echo ""
